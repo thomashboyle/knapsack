97ed15f44fc9fc16a7d0359b51446c98ffe28890
thomashboyle <thomas.h.boyle@gmail.com>
2023-10-03 19:50:31 -0600

    Approximation Algorithms Completed

--- algorithms.py
+++ algorithms.py
@@ -56,34 +56,41 @@ def bellman_recursion_decision(itemSet, capacity):
 
 # returns profit with given items and decisions
 def profit_of_set(itemSet, decisionSet, useWeight):
-    print(itemSet, decisionSet)
+    # print("weight" if useWeight else "profit", "of", itemSet, decisionSet, "=", end=" ")
     if type(decisionSet) is int:
-        print(bin(decisionSet))
+        # print(bin(decisionSet))
         total = 0
         i = len(itemSet) - 1
         while decisionSet != 0:
             total += (decisionSet & 1)*itemSet[i][useWeight]
             i -= 1
             decisionSet >>= 1
+        # print(total)
         return total
     else:
+        # print(sum([item[useWeight] * int(decision) for item, decision in zip(itemSet, decisionSet)]))
         return sum([item[useWeight] * int(decision) for item, decision in zip(itemSet, decisionSet)])
 
 
 # figure 2.2
 def DP1(itemSet, capacity):
     dpTable = []
+    #initialize table to 0
     for i in range(len(itemSet) + 1):
         dpTable += [[0]*(capacity+1)]
+    # loop through items
     for (i, (profit, weight)) in enumerate(itemSet):
-        for subProblemCapacity in range(1,weight): # item cannot be packed
+        # fill out subproblem profit if item cant be packed
+        for subProblemCapacity in range(1,weight):
             dpTable[i+1][subProblemCapacity] = dpTable[i][subProblemCapacity]
-        for subProblemCapacity in range(weight, capacity+1): # item can be packed
+        # decide whether to add it if it can fit
+        for subProblemCapacity in range(weight, capacity+1):
             dpTable[i+1][subProblemCapacity] = max(dpTable[i][subProblemCapacity - weight] + profit,
                                                    dpTable[i][subProblemCapacity])
     return dpTable
 
 
+# DP1 that keeps track of decisions
 def DPdecisions(itemSet, capacity):
     dpTable = []
     decisionTable = []
@@ -106,9 +113,12 @@ def DPdecisions(itemSet, capacity):
 
 # figure 2.4
 def DP2(itemSet, capacity):
+    # only keep track of one row at a time
     dpTable = [0]*(capacity+1)
 
+    # overwrite row for each item
     for profit, weight in itemSet:
+        # fill out row from high to low capacity
         for subProblemCapacity in range(capacity, weight-1, -1):  # item can be packed
             dpTable[subProblemCapacity] = max(dpTable[subProblemCapacity - weight] + profit,
                                                    dpTable[subProblemCapacity])
@@ -119,6 +129,7 @@ def DP3(itemSet, capacity):
     solutionProfit = 0
     numItems = len(itemSet)
     iterationCapacity = capacity
+    # run DP2 for each item in solution set (so dont have to store decisions)
     while iterationCapacity >= min(itemSet, key=lambda x: x[1])[1]:
         profitTable = [0] * (capacity + 1)
         itemTable = [0] * (capacity + 1)
@@ -155,3 +166,54 @@ def branch_and_bound(itemSet, capacity, branchAt=0, branchDecisions=0, bestProfi
         bestProfit, bestDecisions = branch_and_bound(itemSet, capacity, branchAt + 1, branchDecisions << 1, bestProfit, bestDecisions)
     return bestProfit, bestDecisions
 
+
+# extended greed is at least half of optimal profit
+def extended_greedy(itemSet, capacity):
+    # run greedy heuristic
+    greedyDecisions = greedy_heuristic(itemSet, capacity)
+    # find item with highest profit
+    maxItem = max(enumerate(itemSet), key=lambda x: x[1][0])
+    # choose higher of two profits
+    if profit_of_set(itemSet, greedyDecisions, False) > maxItem[1][0]:
+        return greedyDecisions
+    else:
+        return [0]*(maxItem[0]) + [1] + [0]*(len(itemSet) - maxItem[0] - 1)
+
+
+# extended greedy improved is at least 3/4 of optimal profit
+def extended_greedy_improved(itemSet, capacity):
+    maxItem = max(enumerate(itemSet), key=lambda x: x[1][0])
+    bestSolution = [0] * (maxItem[0]) + [1] + [0] * (len(itemSet) - maxItem[0] - 1)
+    bestProfit = maxItem[1][0]
+    # choose all pairs
+    for i, (profiti, weighti) in enumerate(itemSet):
+        for j, (profitj, weightj) in enumerate(itemSet[i+1:]):
+            print("Pair chosen:", i, j, (profiti, weighti), (profitj, weightj))
+            # if pair fits in bag
+            if weighti + weightj <= capacity:
+                # find subset of items left that can fit in remaining capacity and have less profit than both
+                subsetCapacity = capacity - weighti - weightj
+                subset = [x for x in itemSet if x[0] < min(profiti, profitj) and x[1] <= subsetCapacity]
+                subsetIndices  = [i for i, x in enumerate(itemSet) if x[0] < min(profiti, profitj) and x[1] <= subsetCapacity]
+
+                # if none left
+                if len(subset) == 0:
+                    # check if better profit
+                    if profiti + profitj > bestProfit:
+                        bestSolution = [0]*len(itemSet)
+                        bestSolution[i] = 1
+                        bestSolution[j + i + 1] = 1
+                        bestProfit = profiti + profitj
+                    continue
+
+                # run extended greedy algorithm on remaining items
+                subsetSolution = extended_greedy(subset, subsetCapacity)
+                subsetProfit = profit_of_set(subset, subsetSolution, False)
+
+                if profiti + profitj + subsetProfit > bestProfit:
+                    bestProfit = profiti + profitj + subsetProfit
+                    bestSolution = [1 if i in subsetIndices and subsetSolution[subsetIndices.index(i)]
+                                    else 0 for i, _ in enumerate(itemSet)]
+                    bestSolution[i] = 1
+                    bestSolution[i + j + 1] = 1
+    return bestSolution
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# This is a sample Python script.
-
 import algorithms
 
 def print_table(table):
@@ -12,6 +10,7 @@ def print_decision_table(table):
 items1 = [(9, 7),  (7, 9), (8, 6), (6, 2), (6, 5), (5, 3), (3, 4)]
 itemstxtbk = [(6, 2),  (5, 3), (8, 6), (9, 7), (6, 5), (7, 9), (3, 4)]
 itemstxtbk2 = [(3, 4),  (5, 3), (8, 6), (9, 7), (6, 5), (6, 2), (7, 9)]
+itemsTricky = [(2, 1), (20,20)]
 
 decisionsi1c9 = [0, 0, 0, 1, 0, 1, 1]
 
@@ -25,5 +24,5 @@ print_decision_table(decisions)
 print(algorithms.DP2(itemstxtbk, 9))
 print(algorithms.DP3(itemstxtbk, 9))
 print(algorithms.branch_and_bound(itemstxtbk, 9))
-
-
+print(algorithms.extended_greedy(itemsTricky, 20))
+print(algorithms.extended_greedy_improved(items1, 9))
